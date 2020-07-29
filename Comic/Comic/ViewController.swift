ce7ae21d974c59b2abb7163de2b123464f114f6c
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-29 14:25:02 -0500

    Add files via upload

---
+++ Comic/Comic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comic
+//
+//  Created by Raymond Lo on 7/29/20.
+//  Copyright © 2020 Raymond Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Comic/ComicTests/ComicTests.swift
@@ -0,0 +1,34 @@
+//
+//  ComicTests.swift
+//  ComicTests
+//
+//  Created by Raymond Lo on 7/29/20.
+//  Copyright © 2020 Raymond Lo. All rights reserved.
+//
+
+import XCTest
+@testable import Comic
+
+class ComicTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
